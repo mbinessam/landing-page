bf1e1923987004c67d7aa14e66b860b3a34a1ee1
mohammadessam <zmohammadessam@gmail.com>
2021-07-13 20:00:43 +0200

    Initial upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# Landing Page Project
+
+## Description
+
+This landing page project has been created using raw technologies, i.e HTML, CSS, and Vanilla JavaScript for Udacity and egFWD.
+
+### What to expect:
+
+The page has specific tasks to do:
+
+- Build navigation dynamically,
+- Adding active state to section in viewport
+- Page should scroll smoothly (through javascript not css)
+
+### Used Technologies
+
+- HTML
+- CSS
+- JavaScript (Vanilla)
+
+```
+Version: 1.0
+Authored By: Mohammad Essam
+```
+
+The JavaScript of this site has been coded by Mohammad Essam
---
+++ js/app.js
@@ -0,0 +1,80 @@
+// General Constants
+const sections = document.querySelectorAll("section");
+const navMenu = document.querySelector("ul#navbar__list");
+
+// Check if section is in viewport or not
+findInView = (section) => {
+  // Resource: https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
+  let sectionCords = section.getBoundingClientRect();
+  if (
+    sectionCords.top >= 0 &&
+    sectionCords.left >= 0 &&
+    sectionCords.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    sectionCords.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  )
+    return true;
+};
+
+// Building navigation bar and smooth scroll functionality
+buildNav = () => {
+  sections.forEach((section) => {
+    let navLinkItem = document.createElement("li");
+    let navLink = document.createElement("a");
+    navLink.classList.add("menu__link");
+    navLink.innerHTML = section.dataset.nav;
+    navLink.setAttribute("href", `#${section.id}`);
+    navLinkItem.appendChild(navLink);
+    navMenu.appendChild(navLinkItem);
+
+    // Smooth scroll
+    navLink.addEventListener("click", function (e) {
+      e.preventDefault();
+      section.scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+      });
+    });
+  });
+};
+
+// Adding active class to section in viewport
+addActiveClass = () => {
+  document.addEventListener("scroll", () => {
+    sections.forEach((section) => {
+      if (findInView(section)) {
+        section.classList.add("your-active-class");
+      } else if (section.classList.contains("your-active-class")) {
+        section.classList.remove("your-active-class");
+      }
+    });
+  });
+};
+
+// Extra: Adding jump to top button
+scrollToTop = () => {
+  let toTop = document.querySelector(".top");
+  document.addEventListener("scroll", () => {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      toTop.style.display = "block";
+    } else {
+      toTop.style.display = "none";
+    }
+  });
+
+  toTop.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+};
+
+// Calling functions
+buildNav();
+addActiveClass();
+scrollToTop();
